aedcefa0a3d302d472ce0afde347cd717ac3e967
T.MAHARANI <maharani1118@gmail.com>
2023-11-10 20:03:14 +0530

    Create 03_11_2023.cpp
    Problem Of the Day 03/11/2023

---
+++ 03_11_2023.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+bool splitString(string &str){
+    // Write your code here.
+    int first_half_vowel = 0, second_half_vowel = 0, count = 0, i;
+    for(i=0; str[i] != '\0'; i++) {
+        count++;
+    }
+
+    for(i=0; i<= count; i++) {
+        if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o'  || str[i] == 'u'
+        || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U' ) {
+            if(i< (count/2)) {
+                first_half_vowel += 1;
+            }
+            else {
+                second_half_vowel += 1;
+            }
+        }
+    }
+    
+    if(first_half_vowel == second_half_vowel) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
