56f8de722f5b162ffada34fad8b221e8327c1e10
T.MAHARANI <maharani1118@gmail.com>
2023-11-14 13:51:36 +0530

    Update 11_11_2023.cpp

--- 11_11_2023.cpp
+++ 11_11_2023.cpp
@@ -3,16 +3,14 @@ long long int sumOrProduct(long long int n, long long int q)
 {
 	// Write your code here.
 	long long int res = 0; 
-	
 	if(q == 1) {
 		res = (n * (n+1))/2;
 	}
 	else {
 		res = 1;
 		for(long long int i=1; i<=n; i++){
-			res = (res *i) % (1000000007);
+		   res = (res *i) % (1000000007);
 		}
-		//res = (res) % (1000000007);
 	}
 	return res;
 }
