b370a196c05971e6ae0db27b9cdd5bcfe53a8258
T.MAHARANI <maharani1118@gmail.com>
2023-12-26 13:07:01 +0530

    Problem Of the day 26/12/2023
    Flower Shop

---
+++ dec-2023/26_12_2023.cpp
@@ -0,0 +1,10 @@
+//Flower Shop
+#include <bits/stdc++.h> 
+long long evenSumTillN(int n) {
+    // Write your code here.
+    long long res = 0;
+    for(int i=2; i<=n; i+=2) {
+        res += i;
+    }
+    return res;
+}
