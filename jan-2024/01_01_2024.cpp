4c456660deea69ad4ce335e07809d35d855f9ea4
T.MAHARANI <maharani1118@gmail.com>
2024-01-01 11:54:40 +0530

    Problem Of the day 01/01/2024.cpp
    Check Even Partitioning

---
+++ jan-2024/01_01_2024.cpp
@@ -0,0 +1,13 @@
+//Check Even Partitioning 
+#include <bits/stdc++.h> 
+bool checkEvenPartitioning(int n) 
+{
+    // Write your code here
+    if((n>2) && (n%2==0)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
