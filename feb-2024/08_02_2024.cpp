2f4df4f2a1d51f98ef6816a40e5503cab7b6a049
T.MAHARANI <maharani1118@gmail.com>
2024-02-08 12:19:26 +0530

    Problem Of the day 08/02/2024
    sortBinaryArray

---
+++ feb-2024/08_02_2024.cpp
@@ -0,0 +1,22 @@
+//sortBinaryArray
+
+vector<int> sortBinaryArray(vector<int> arr, int n)  {
+ 	// Write your code here
+	int num_zero_count=0;
+
+	for(int i=0; i<n; i++) {
+		if(arr[i] == 0) {
+			num_zero_count += 1;
+		}
+	}
+
+	for(int i=0; i<n; i++) {
+		if(num_zero_count-- > 0) {
+			arr[i] = 0;
+		}
+		else {
+			arr[i] = 1;
+		}
+	}
+	return arr;
+}
