810a0dbcb4ec291443502da94bafdcef32ede2b8
T.MAHARANI <maharani1118@gmail.com>
2024-02-09 16:20:03 +0530

    Problem of the day 09/02/2024
    Reverse The Array

---
+++ feb-2024/09_02_2024.cpp
@@ -0,0 +1,12 @@
+//reverseArray
+
+#include <bits/stdc++.h> 
+void reverseArray(vector<int> &arr , int m)
+{
+	// Write your code here.
+	for(int i=m+1, j=arr.size()-1; i<j; i++, j--) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+}
