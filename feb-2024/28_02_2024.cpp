8e81bb20b73107b75f7aeaab5b82a1cc99f5efd7
T.MAHARANI <maharani1118@gmail.com>
2024-02-29 20:06:56 +0530

    Problem Of the Day 28/02/2024
    printNos

---
+++ feb-2024/28_02_2024.cpp
@@ -0,0 +1,9 @@
+//printNos
+vector<int> printNos(int x) {
+    // Write Your Code Here
+    vector<int> result;
+    for(int i=1; i<=x; i++) {
+        result.push_back(i);
+    }
+    return result;
+}
