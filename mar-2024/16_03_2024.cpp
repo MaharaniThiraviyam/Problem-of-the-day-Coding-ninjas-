fcc742b76a0beb5972c0339ff12605e63249ffb7
T.MAHARANI <maharani1118@gmail.com>
2024-03-18 21:19:51 +0530

    cherry Biscuit
    Problem Of the Day 16/03/2024

---
+++ mar-2024/16_03_2024.cpp
@@ -0,0 +1,11 @@
+//cherryBiscuit
+int cherryBiscuit(vector<int> &a, int n, int k) {
+    // Write your code here.
+    int counter = 0;
+    for(int i=0; i<n; i++) {
+        if(a[i] > k) {
+            counter++;
+        }
+    }
+    return counter;
+}
