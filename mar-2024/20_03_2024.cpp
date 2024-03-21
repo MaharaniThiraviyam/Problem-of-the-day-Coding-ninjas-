1834e0dd331f531de1118f11214b4d8dbb24c995
T.MAHARANI <maharani1118@gmail.com>
2024-03-21 20:12:24 +0530

    getFloorAndCeil
    Problem Of the Day 20/03/2024

---
+++ mar-2024/20_03_2024.cpp
@@ -0,0 +1,22 @@
+//getFloorAndCeil
+pair<int, int> getFloorAndCeil(vector<int> &arr, int n, int x) {
+	// Write your code here.
+	int ceil = -1;
+	int floor = -1;
+
+	for(int i=0; i<n; i++) {
+		if(arr[i] == x) {
+			ceil = x;
+			floor = x;
+			break;
+		}
+		else if(arr[i]>x && (floor == -1 || arr[i]<floor)) {
+			floor = arr[i];
+		}
+		else if(arr[i]<x && arr[i]>ceil) {
+			ceil = arr[i];
+		}
+
+	}
+	return make_pair(ceil, floor);
+}
