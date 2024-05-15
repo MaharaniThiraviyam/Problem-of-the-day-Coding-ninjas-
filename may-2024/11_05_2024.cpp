458d1a24e6210258e02a91319aacf4719b63263c
T.MAHARANI <maharani1118@gmail.com>
2024-05-15 19:25:39 +0530

    valueEqualToIndex
    Problem Of the Day 11/05/2024

---
+++ may-2024/11_05_2024.cpp
@@ -0,0 +1,12 @@
+//valueEqualToIndex
+
+	vector<int> valueEqualToIndex(int arr[], int n) {
+	    // code here
+	    vector<int>result;
+	    for(int i=0; i<n; i++) {
+	        if(arr[i] == (i+1)) {
+	            result.push_back(i+1);
+	        }
+	    }
+	    return result;
+	}
