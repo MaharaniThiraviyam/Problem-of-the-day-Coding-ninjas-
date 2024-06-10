fb4f1df321596e95165dd189019d5ca4614a90c9
T.MAHARANI <maharani1118@gmail.com>
2024-06-10 20:24:31 +0530

    odd or even
    problem of the day 06/06/2024

---
+++ june-2024/06_06_2024.cpp
@@ -0,0 +1,15 @@
+//odd or even
+#include<iostream>
+using namespace std;
+
+int main() {
+	// Write your code here
+	int N;
+	std::cin>>N;
+	if(N%2 == 0) {
+		std::cout<<"Even";
+	}
+	else {
+		std::cout<<"Odd";
+	}
+}
