e025e90a40e278d2cd3bc1844a858f544c22b2cb
T.MAHARANI <maharani1118@gmail.com>
2024-06-14 20:34:15 +0530

    armstrongNumber
    Problem of the day 12/06/2024

---
+++ june-2024/12_06_2024.cpp
@@ -0,0 +1,19 @@
+//armstrongNumber
+    string armstrongNumber(int n) {
+        // code here
+        
+        long long result = 0;
+        int temp = n;
+        for(; n>0; n/=10) {
+            int rem = n%10;
+            
+            result += (rem * rem * rem);
+        }
+        
+        if(temp == result) {
+            return "true";
+        }
+        else {
+            return "false";
+        }
+    }
