72c8ae5ab00278394e73136267195f4edc816f55
T.MAHARANI <maharani1118@gmail.com>
2024-08-13 19:32:23 +0530

    Searching a number
    Problem Of the day 20/07/2024

---
+++ july-2024/20_07_2024.cpp
@@ -0,0 +1,10 @@
+//Searching a number
+ int search(int k, vector<int>& arr) {
+        // code here
+        for(int i=0; i<arr.size(); i++) {
+            if(arr[i] == k) {
+                return i+1;
+            }
+        }
+        return -1;
+    }
