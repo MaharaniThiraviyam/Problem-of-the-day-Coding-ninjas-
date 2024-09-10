816127de4c704c0f5d6bd92a013ea8e7612a6c6b
T.MAHARANI <maharani1118@gmail.com>
2024-09-10 22:56:15 +0530

    reverse
    Problem Of the day 27/07/2024

---
+++ july-2024/27_07_2024.cpp
@@ -0,0 +1,8 @@
+
+   void reverse(int arr[], int start, int end) {
+        while (start < end) {
+            swap(arr[start], arr[end]);
+            start++;
+            end--;
+        }
+    }
