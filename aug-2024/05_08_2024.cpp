0ceb0924ddfc69c9661af3e491b0b065abbc589d
T.MAHARANI <maharani1118@gmail.com>
2024-09-16 23:01:32 +0530

    countSquares
    Problem of the day 05/08/2024

---
+++ aug-2024/05_08_2024.cpp
@@ -0,0 +1,12 @@
+//countSquares
+ int countSquares(int N) {
+        // code here
+        
+        if (N <= 1) {
+            return 0;
+        }
+        
+        int k = sqrt(N - 1);
+        
+          return k;
+    }
