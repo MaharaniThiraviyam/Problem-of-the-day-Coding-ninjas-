248061e7f4cb2aec0a36238fcd0d5bbd6f500214
T.MAHARANI <maharani1118@gmail.com>
2024-10-07 19:34:30 +0530

    find_median
    Problem Of the day 28/08/2024

---
+++ aug-2024/28_08_2024.cpp
@@ -0,0 +1,16 @@
+int find_median(vector<int> arr)
+		{
+		    // Code here.
+		    int n = arr.size();
+    
+            sort(arr.begin(), arr.end());
+            
+            if (n % 2 != 0) {
+                return arr[n / 2];
+            } 
+            else {
+                int mid1 = arr[(n / 2) - 1];
+                int mid2 = arr[n / 2];
+                return (mid1 + mid2) / 2;
+            }
+		}
